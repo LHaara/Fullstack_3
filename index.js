38e0455a0fb24254895701645ad0bd20efe49b30
Lauri Haara <lauri.haara@gmail.com>
2018-11-11 20:50:36 +0200

    3_5

--- index.js
+++ index.js
@@ -65,6 +65,18 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  person.id = Math.floor((Math.random() * 1000000) + 1)
+
+  console.log(person.id)
+
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
