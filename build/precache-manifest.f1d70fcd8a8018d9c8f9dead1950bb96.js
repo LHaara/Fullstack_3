189785df8e35bb6a1c80290f92274257754078c9
Lauri Haara <lauri.haara@gmail.com>
2018-11-17 17:22:33 +0200

    frontend tuotantoon

---
+++ build/precache-manifest.f1d70fcd8a8018d9c8f9dead1950bb96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c13b21cbbca4ab37cd96",
+    "url": "/static/js/main.c13b21cb.chunk.js"
+  },
+  {
+    "revision": "b08413150464f681f98f",
+    "url": "/static/js/1.b0841315.chunk.js"
+  },
+  {
+    "revision": "c13b21cbbca4ab37cd96",
+    "url": "/static/css/main.a4383642.chunk.css"
+  },
+  {
+    "revision": "b29578efa9c7258355486a9cdd4cced3",
+    "url": "/index.html"
+  }
+];
